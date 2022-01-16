dd04431c467dddf70f5b08d01e016c0422a80c3c
maisahr <ma.isahr7@gmail.com>
2022-01-15 22:15:36 -0600

    Cifrando una letra

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+function displayCifrar(){
+    document.getElementById("secDescifrar").style.display = "none"
+    document.getElementById("secCifrar").style.display = "block"
+}
+
+let btnParaCifrar = document.getElementById("btnParaCifrar")
+
+btnParaCifrar.addEventListener("click", displayCifrar)
+
+function displayDescifrar(){
+    document.getElementById("secCifrar").style.display = "none"
+    document.getElementById("secDescifrar").style.display = "block"
+}
+
+let btnParaDescifrar = document.getElementById("btnParaDescifrar")
+
+btnParaDescifrar.addEventListener("click", displayDescifrar)
+
+function cifrar(){
+    let mensajeCifrar = document.getElementById("mensajeCifrar").value
+    let offset = Number(document.getElementById("offset").value)
+    let letraASCII = Number(mensajeCifrar.charCodeAt())
+    let formula = ((letraASCII - 65) + offset) % 26 + 65
+    let letraNueva = String.fromCharCode(formula)
+    document.getElementById("letra").innerHTML = letraNueva
+}
+
+let btnCifrar = document.getElementById("cifrar")
+
+btnCifrar.addEventListener("click", cifrar)
