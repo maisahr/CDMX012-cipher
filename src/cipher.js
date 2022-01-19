5d64ee1b64fe00cf940460cf41e932f395c6dee8
maisahr <ma.isahr7@gmail.com>
2022-01-19 10:55:03 -0600

    Cifra correctamente y descifra con errores

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,13 @@
 const cipher = {
+    encode : function(a, b){
+
+      return ((a - 65) + b) % 26 + 65 // Ciframos ASCII a ASCII
+ 
+    },
+
+    decode : function(x, y){
+      return ((x - 65) - y) % 26 + 65
+    },
 
 };
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
-
 function displayCifrar(){
     document.getElementById("secDescifrar").style.display = "none"
     document.getElementById("secCifrar").style.display = "block"
 }
 
 let btnParaCifrar = document.getElementById("btnParaCifrar")
-
 btnParaCifrar.addEventListener("click", displayCifrar)
 
 function displayDescifrar(){
@@ -17,18 +14,53 @@ function displayDescifrar(){
 }
 
 let btnParaDescifrar = document.getElementById("btnParaDescifrar")
-
 btnParaDescifrar.addEventListener("click", displayDescifrar)
 
 function cifrar(){
+    let i = 0
     let mensajeCifrar = document.getElementById("mensajeCifrar").value
     let offset = Number(document.getElementById("offset").value)
-    let letraASCII = Number(mensajeCifrar.charCodeAt())
-    let formula = ((letraASCII - 65) + offset) % 26 + 65
-    let letraNueva = String.fromCharCode(formula)
-    document.getElementById("letra").innerHTML = letraNueva
+    let acumulaMensaje = ""
+
+    while(i < mensajeCifrar.length){
+        let letra = mensajeCifrar.slice(i, i + 1)
+        let letraASCII = Number(letra.charCodeAt())
+        let formula = cipher.encode(letraASCII, offset)
+        let letraNueva = String.fromCharCode(formula)
+        acumulaMensaje += letraNueva
+        i ++
+        
+        if(i == mensajeCifrar.length){
+            document.getElementById("letra").innerHTML = acumulaMensaje
+        }
+    }
 }
 
 let btnCifrar = document.getElementById("cifrar")
-
 btnCifrar.addEventListener("click", cifrar)
+
+function descifrar(){
+    let i = 0
+    let mensajeDescifrar = document.getElementById("mensajeDescifrar").value
+    let offsetD = Number(document.getElementById("offsetD").value)
+    let acumulaMensaje = ""
+
+    while(i < mensajeDescifrar.length){
+        let letra = mensajeDescifrar.slice(i, i + 1)
+        let letraASCII = Number(letra.charCodeAt())
+        let formula = cipher.decode(letraASCII, offsetD)
+        let letraNueva = String.fromCharCode(formula)
+        acumulaMensaje += letraNueva
+        i ++
+
+        if(i == mensajeDescifrar.length){
+            document.getElementById("letraD").innerHTML = /* "Letra ASCII = " + letraASCII
+            + "\n Resultado formula = " + formula 
+            + "\n Mensaje = " + */ acumulaMensaje
+        }
+    }
+}
+
+let btnDescifrar = document.getElementById("descifrar")
+
+btnDescifrar.addEventListener("click", descifrar)
